3c9d24c57d0c80b7caf474a7e0a90e785c8c87e7
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-10-25 22:30:07 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /etc/backup/backup.conf
+hely=$utvonal/`date +%Y-%m-%d`/`date +%H-%M`
+sudo mkdir -p $hely
+sudo echo `date` Biztonságimentés elkezdve... >>$hely/backup.log
+sudo dd bs=16M if=/dev/mmcblk0 status=progress iflag=nofollow,noatime | gzip > $hely/raspbian-`date +%Y-%m-%d`.img.gz && sudo echo `date` Biztonságimentés sikeres! >>$hely/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>$hely/backup.log
+sudo ls -l -h $hely/raspbian-`date +%Y-%m-%d`.img.gz
