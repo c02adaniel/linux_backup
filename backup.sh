620ac417966d2172576704b1c101135928f48f98
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-03 23:15:56 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@ source /etc/backup/backup.conf
 hely=$utvonal/`date +%Y-%m-%d`/`date +%H-%M`
 sudo mkdir -p $hely
 sudo echo `date` Biztonságimentés elkezdve... >>$hely/backup.log
-sudo dd bs=16M if=/dev/mmcblk0 status=progress iflag=nofollow,noatime | gzip > $hely/raspbian-`date +%Y-%m-%d`.img.gz && sudo echo `date` Biztonságimentés sikeres! >>$hely/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>$hely/backup.log
-sudo ls -l -h $hely/raspbian-`date +%Y-%m-%d`.img.gz
+sudo dd bs=16M if=/dev/mmcblk0 status=progress iflag=nofollow,noatime | gzip > $hely/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>$hely/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>$hely/backup.log
+sudo ls -l -h $hely/backup.img.gz
