19002ff7790b44da4b81101b12c0ae4546cb65ac
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-03 22:20:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 config_hely="/etc/backup/backup.conf"
 config_tartalom="utvonal="
-sudo mkdir /etc/backup
-sudo cp backup.sh /etc/backup/
-sudo ln -s /etc/backup/backup.sh /bin/backup
+install_loc=`dirname "$BASH_SOURCE"`
+sudo mkdir -p /etc/backup
+sudo cp $install_loc/backup.sh /etc/backup/
+link=/bin/backup
+if [ ! -f $link ]; then
+  #echo "=> File doesn't exist"
+  sudo ln -s /etc/backup/backup.sh /bin/backup
+fi
+sudo chmod +x /etc/backup/backup.sh
+sudo chmod +x /bin/backup
 echo
 echo -e "Kérlek add meg azt a helyet ahova a biztonsági másolatok készülni fognak!\nFONTOS: A végére nem kell perjel!\nPéldául: /media/data"
 read -p "Elérésiút: " utvonal
