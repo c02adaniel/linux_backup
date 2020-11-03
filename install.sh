c00ff47438eda8407d8ccddccee19d6903e30937
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-03 23:41:28 +0100

    Update install.sh
    A telepítő még nem ellenőrzi, hogy a biztonsági mentés helye létezik-e, illetve, hogy megfelelő-e az eszköznév.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 config_hely="/etc/backup/backup.conf"
-config_tartalom="utvonal="
+config_utvonal="utvonal="
+config_mirol="mirol="
 install_loc=`dirname "$BASH_SOURCE"`
 sudo mkdir -p /etc/backup
 sudo cp $install_loc/backup.sh /etc/backup/
@@ -14,5 +15,9 @@ sudo chmod +x /bin/backup
 echo
 echo -e "Kérlek add meg azt a helyet ahova a biztonsági másolatok készülni fognak!\nFONTOS: A végére nem kell perjel!\nPéldául: /media/data"
 read -p "Elérésiút: " utvonal
-echo $config_tartalom$utvonal >> $config_hely
+echo $config_utvonal$utvonal >> $config_hely
+echo
+echo -e "Kérlek add meg, hogy miről készül biztonsági másolat!\nCsak az eszköz nevét! Például: sda, sda0, sdb, sdb0, stb.\nA memóriakártya neve kisgépeknél (Raspbian): mmcblk0"
+read -p "Név: " mirol
+echo $config_mirol$mirol >> $config_hely
 echo
