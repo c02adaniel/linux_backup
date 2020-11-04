4ba1e9d1ae72395fac8537fce9b188443522b111
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-04 22:28:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-config_hely="/etc/backup/backup.conf"
-config_utvonal="utvonal="
-config_mirol="mirol="
+config_location="/etc/backup/backup.conf"
+config_text1="utvonal="
+config_text2="mirol="
 install_loc=`dirname "$BASH_SOURCE"`
 sudo mkdir -p /etc/backup
-sudo cp $install_loc/backup.sh /etc/backup/
+sudo cp ${install_loc}/backup.sh /etc/backup/
 link=/bin/backup
-if [ ! -f $link ]; then
+if [ ! -f ${link} ]; then
   #echo "=> File doesn't exist"
   sudo ln -s /etc/backup/backup.sh /bin/backup
 fi
@@ -14,11 +14,11 @@ sudo chmod +x /etc/backup/backup.sh
 sudo chmod +x /bin/backup
 echo
 echo -e "Kérlek add meg azt a helyet ahova a biztonsági másolatok készülni fognak!\nFONTOS: A végére nem kell perjel!\nPéldául: /media/data"
-read -p "Elérésiút: " utvonal
-echo $config_utvonal$utvonal >> $config_hely
-sudo touch $utvonal/backup.ready
+read -p "Elérésiút: " location
+echo ${config_text1}${location} >> ${config_location}
+sudo touch ${location}/backup.ready
 echo
 echo -e "Kérlek add meg, hogy miről készül biztonsági másolat!\nCsak az eszköz nevét! Például: sda, sda0, sdb, sdb0, stb.\n Raspberry Pi-knél (Raspbian OS) a memóriakártya neve: mmcblk0"
-read -p "Név: " mirol
-echo $config_mirol$mirol >> $config_hely
+read -p "Név: " source_dev
+echo ${config_text2}${source_dev} >> ${config_location}
 echo
