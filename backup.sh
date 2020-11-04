dfca3e75e4e440d8dbd598ca13b4e299b83a9006
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-04 23:02:08 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ if [[ -f ${link} ]]; then
 target=${location}/`date +%Y-%m-%d`/`date +%H-%M`
 sudo mkdir -p ${target}
 sudo echo `date` Biztonságimentés elkezdve... >>${target}/backup.log
-sudo dd bs=16M if=/dev/${source_dev} status=progress iflag=nofollow,noatime | gzip > ${target}/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>${target}/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>${target}/backup.log
+sudo dd bs=${bs_var} if=/dev/${source_dev} status=${status_var} iflag=${iflag_var} ${other_var} | gzip > ${target}/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>${target}/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>${target}/backup.log
 sudo ls -l -h ${target}/backup.img.gz
 else
 echo "Nem található a biztonsági mentés cél helye. Csatlakoztasd a meghajtót, vagy állíts be egy új célállomást az install.sh segítségével!"
