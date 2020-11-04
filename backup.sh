c8b6edd5338934906e7f58e18a5350cfb83e3010
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2020-11-04 22:41:07 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source /etc/backup/backup.conf
-link=$utvonal/backup.ready
-if [ -f $link ]; then
-hely=$utvonal/`date +%Y-%m-%d`/`date +%H-%M`
-sudo mkdir -p $hely
-sudo echo `date` Biztonságimentés elkezdve... >>$hely/backup.log
-sudo dd bs=16M if=/dev/$mirol status=progress iflag=nofollow,noatime | gzip > $hely/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>$hely/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>$hely/backup.log
-sudo ls -l -h $hely/backup.img.gz
+link=${location}/backup.ready
+if [[ -f ${link} ]]; then
+target=${location}/`date +%Y-%m-%d`/`date +%H-%M`
+sudo mkdir -p ${target}
+sudo echo `date` Biztonságimentés elkezdve... >>${target}/backup.log
+sudo dd bs=16M if=/dev/${source_dev} status=progress iflag=nofollow,noatime | gzip > ${target}/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>${target}/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>${target}/backup.log
+sudo ls -l -h ${target}/backup.img.gz
 else
 echo "Nem található a biztonsági mentés cél helye. Csatlakoztasd a meghajtót, vagy állíts be egy új célállomást az install.sh segítségével!"
 fi
