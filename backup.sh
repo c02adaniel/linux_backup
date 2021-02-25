1f182bad48b6cafce3e96ec6d8895f46a9ffd80c
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2021-02-25 16:00:00 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ echo -e "\n"`date`"\n"
 target=${location}/`date +%Y-%m-%d`/`date +%H-%M`
 sudo mkdir -p ${target}
 sudo echo `date` Biztonságimentés elkezdve... >>${target}/backup.log
-sudo dd bs=${bs_var} if=/dev/${source_dev} status=${status_var} iflag=${iflag_var} ${other_var} | gzip > ${target}/backup.img.gz && sudo echo `date` Biztonságimentés sikeres! >>${target}/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>${target}/backup.log
+sudo (dd bs=${bs_var} if=/dev/${source_dev} iflag=${iflag_var} ${other_var} | gzip > ${target}/backup.img.gz) 2&gt;&amp;1 | dialog --gauge "Running dd command (cloning), please wait..." 10 70 0 && sudo echo `date` Biztonságimentés sikeres! >>${target}/backup.log || sudo echo `date` Biztonságimentés sikertelen! >>${target}/backup.log
 sudo ls -l -h ${target}/backup.img.gz
 echo -e "\n"`date`"\n"
 else
