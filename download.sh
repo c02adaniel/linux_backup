837acc9ab818ad76979dacfeb27335385a8fa9be
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2021-05-13 23:15:10 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+sudo apt install gzip git -y
+cd ~
+git clone https://github.com/c02adaniel/linux_backup.git
+chmod +x linux_backup/*.sh
+sudo linux_backup/install.sh
+cd ..
+sudo rm -rf linux_backup
