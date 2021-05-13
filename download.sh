b72fca7f570f14293e76d6318d17b49c8251a1b7
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2021-05-14 00:56:16 +0200

    Delete download.sh

--- download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo apt update
-#sudo apt install gzip git -y
-cd ~
-git clone https://github.com/c02adaniel/linux_backup.git
-chmod +x linux_backup/*.sh
-sudo linux_backup/install.sh
