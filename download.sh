bdccbf68ee957947e32f60acb8bcb01c5ac01a15
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2021-05-13 23:37:30 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install gzip git -y
+#sudo apt update
+#sudo apt install gzip git -y
 cd ~
 git clone https://github.com/c02adaniel/linux_backup.git
 chmod +x linux_backup/*.sh
