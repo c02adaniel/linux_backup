b0de71ac8175b6bb620259b73fa800591505b2da
c02adaniel <32295203+c02adaniel@users.noreply.github.com>
2021-05-13 23:51:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ config_text5="iflag_var="
 config_text6="other_var="
 install_loc=`dirname "$BASH_SOURCE"`
 sudo apt update
-#sudo apt install pv dialog -y
+sudo apt install pv gzip -y
+wget https://raw.githubusercontent.com/c02adaniel/linux_backup/main/backup.sh
 sudo mkdir -p /etc/backup
 sudo rm -f /etc/backup/backup.sh
 sudo rm -f /etc/backup/backup.conf
